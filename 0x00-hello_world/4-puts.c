bf828164f03179317e31f5bf6ac59d7b4500c9f2
Pauline Kiama <paulinewachoro22@gmail.com>
2023-08-28 12:20:34 +0000

    new code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 /**
- * fisrt code
- */
-int main(void) 
+ *  * Main enTry POint
+ *   *
+ *    * rE:turn: Always 0 (Success)
+ *     */
+int main(void)
 {
-	puts("\""Programming is like building a multilingual puzzle");
-	return (0);
-}
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+} 
